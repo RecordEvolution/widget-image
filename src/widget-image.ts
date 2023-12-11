514712316dcf84ebabac4947bd84035646a5dba5
Marko Petzold <marko.petzold@record-evolution.de>
2023-12-11 23:00:07 +0100

    title padding

--- src/widget-image.ts
+++ src/widget-image.ts
@@ -22,23 +22,28 @@ export class WidgetImage extends LitElement {
     flex-direction: column;
     height: 100%;
     width: 100%;
+    padding: 16px;
+    box-sizing: border-box;
   }
 
   h3 {
-    margin: 0;
-    max-width: 300px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    color: var(--re-user-h2-color, --re-text-color, #000);
-  }
-p {
-    margin: 10px 0 0 0;
-    max-width: 300px;
-    font-size: 14px;
-    line-height: 17px;
-    color: var(--re-user-p-color, --re-text-color, #000);
-  }
+      margin: 0;
+      max-width: 300px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      color: var(--re-text-color, #000) !important;
+    }
+  p {
+      margin: 10px 0 0 0;
+      max-width: 300px;
+      font-size: 14px;
+      line-height: 17px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      color: var(--re-text-color, #000) !important;
+    }
 
   .img-container{
     flex: 1;
@@ -59,10 +64,10 @@ p {
 
     return html`
       <div class="wrapper">
-          <h3 class="paging" ?active=${this.inputData?.title?.text}
+        <h3 class="paging" ?active=${this.inputData?.title?.text}
           style="font-size: ${this.inputData?.title?.fontSize}; 
-            font-weight: ${this.inputData?.title?.fontWeight}; 
-            background-color: ${this.inputData?.title?.backgroundColor};">
+          font-weight: ${this.inputData?.title?.fontWeight}; 
+          background-color: ${this.inputData?.title?.backgroundColor};">
           ${this.inputData?.title?.text}
         </h3>
         <p class="paging" ?active=${this.inputData?.subTitle?.text}
